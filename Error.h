96d779007974702dcc832d7934ce4279504935c9
Ronen <ronensh222@gmail.com>
2024-03-04 23:34:02 +0200

    add error.h

---
+++ Error.h
@@ -0,0 +1,23 @@
+
+#ifndef _ERROR_H_
+#define _ERROR_H_
+
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+typedef enum 
+{
+    ERROR_OK 	 = 0,
+    ERROR_FAIL   = 1,
+   
+} Error_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
